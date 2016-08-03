4bd805f1794461806eadd786acf3836af3aadd2e
Admin <admin@mac-admin.local>
2016-08-03 19:03:47 +0300

    I added post request of registration and JSON recognize  of password received from server

---
+++ WeKast/Import/SwiftHTTP-master/CHANGELOG.md
@@ -0,0 +1,41 @@
+# Change Log
+All notable changes to this project will be documented in this file.
+`SwiftHTTP` adheres to [Semantic Versioning](http://semver.org/).
+
+#### [1.0.5](https://github.com/daltoniam/SwiftHTTP/tree/1.0.5)
+
+Swift 2.2 update.
+
+#### [1.0.4](https://github.com/daltoniam/SwiftHTTP/tree/1.0.4)
+
+fixed:
+[#215](https://github.com/daltoniam/SwiftHTTP/issues/215)
+
+#### [1.0.3](https://github.com/daltoniam/SwiftHTTP/tree/1.0.3)
+
+Changed:
+Add Swift package manager support
+TVOS support
+WatchOS support
+
+fixed:
+[#195](https://github.com/daltoniam/SwiftHTTP/issues/195)
+[#203](https://github.com/daltoniam/SwiftHTTP/issues/203)
+
+#### [1.0.2](https://github.com/daltoniam/SwiftHTTP/tree/1.0.2)
+
+Parameter encoding fixes ([#178](https://github.com/daltoniam/SwiftHTTP/issues/178), [#182](https://github.com/daltoniam/SwiftHTTP/issues/182)).
+Progress Closure.
+[Global handlers](https://github.com/daltoniam/SwiftHTTP#global-handlers).
+
+#### [1.0.1](https://github.com/daltoniam/SwiftHTTP/tree/1.0.1)
+
+Custom header support in `HTTP` factory methods.
+
+#### [1.0.1](https://github.com/daltoniam/SwiftHTTP/tree/1.0.1)
+
+Custom header support in `HTTP` factory methods.
+
+#### [1.0.0](https://github.com/daltoniam/SwiftHTTP/tree/1.0.0)
+
+API refactor and first release of Swift 2 support.
--- WeKast/ViewController.swift
+++ WeKast/ViewController.swift
@@ -2,13 +2,16 @@
 //  ViewController.swift
 //  WeKast
 //
-//  Created by Vladimir Bodovets on 28.07.16.
+//  Created by Vladimir Bodovets on 28.07.16
+//       Upgraded by Igor Belitskii on 03.08.16
 //  Copyright © 2016 WeKast. All rights reserved.
 //
 
 import UIKit
 import AVKit
 import AVFoundation
+import SwiftHTTP
+import JSONJoy
 
 class ViewController: UIViewController {
 
@@ -27,10 +30,65 @@ class ViewController: UIViewController {
     }
     
     @IBAction func showMessage() {
-        let alertMessage = UIAlertController(title: "Окно предупреждений", message: "сообщение с предупреждением", preferredStyle: UIAlertControllerStyle.Alert)
+        let password="";
+        let log=self.login.text;
+        let mail=self.email.text;
+        print(log," ",mail);
+        
+        let params = ["login": log, "email": mail]
+        struct Answer: JSONJoy {
+            let login: String?
+            let email: String?
+            let password : String?
+            init(_ decoder: JSONDecoder){
+                login = decoder["login"].string
+                email = decoder["email"].string
+                password = decoder["password"].string
+            }
+        }
+        struct Response: JSONJoy {
+            let status:  Int?
+            let error: String?
+            let answer : Answer?
+            init(_ decoder: JSONDecoder){
+                status = decoder["status"].integer
+                error = decoder["error"].string
+                answer = Answer(decoder["answer"])
+            }
+        }
+        do {
+            let opt = try HTTP.POST("http://78.153.150.254/register", parameters: params)
+            opt.start { response in
+                print("opt finished: \(response.description)")
+                //  print (response.data)
+                let resp = Response(JSONDecoder(response.data))
+                if let err = resp.error {
+                    print("got an error: \(err)")
+                }
+                if let status = resp.status {
+                    print("code: \(status)")
+                }
+                if let login1 = resp.answer?.login {
+                    print("Registered login: \(login1)")
+                }
+                if let email1 = resp.answer?.email{
+                    print("Registered e-mail: \(email1)")
+                }
+                if let password = resp.answer?.password{
+                    print("Your password: \(password)")
+                    self.passText.text=password
+                }            }
+        } catch let error {
+            print("got an error creating the request: \(error)")
+        }
+        print("Finish");
+        let alertMessage = UIAlertController(title: "Your password", message: "Your password is \(password)", preferredStyle: UIAlertControllerStyle.Alert)
         alertMessage.addAction(UIAlertAction(title: "Продолжить...", style: UIAlertActionStyle.Default, handler: nil))
         self.presentViewController(alertMessage, animated: true, completion: nil)
     }
+    @IBOutlet weak var login: UITextField!
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var passText: UITextField!
     
     @IBAction func startSplashScreen() {
         playLocalVideo()
